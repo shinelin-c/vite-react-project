1f323f307ae745a4b0cf4b693b2b69b103ae77b3
cwl <caiwl@geoscene.cn>
2023-10-30 14:23:31 +0800

    feat():添加less支持

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/*
+ * @Description: 
+ * @Author: caiwl@geoscene.cn
+ * @Date: 2023-10-12 11:31:04
+ */
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './index.less';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -29,7 +34,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
